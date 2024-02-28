d8abc4d97b96551adc327d99bd73b1e89e1b5bc7
Yuyu-san34 <160429198+Yuyu-san34@users.noreply.github.com>
2024-02-28 18:38:05 -0500

    Add files via upload

---
+++ App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args){
+        double suma,Y,X;
+        suma=0;
+        X=20;
+        suma=suma+X;
+        Y=40;
+        X=X+Math.pow(Y, 2);
+        suma=suma+(X/Y);
+    System.out.println("El valor de la suma es:"+suma);
+    }
+}
